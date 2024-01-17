ace1e08d3dc639d5a7468a2eb5cd3027064a3058
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-17 19:46:55 +0600

    metadata added in every page ,, loading page added

---
+++ app/blogs/[id]/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="blogLoadingContainer bg-sky-100 flex justify-center items-center h-screen   ">
+      <p> blog loading ..... </p>
+    </div>
+  );
+};
+
+export default loading;
